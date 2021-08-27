2bed2b3d52c7498dc70348c591b5367577143114
rlagusgh0223 <hopanman11@naver.com>
2021-08-27 16:53:32 +0900

    2021/08/27

---
+++ python/210827_연습.py
@@ -0,0 +1,33 @@
+"""
+#1 Hello, world!두 번 출력
+print("Hello, world!")
+print("Hello, world!")
+
+#2 Hello, world!, 밑에줄에는 LikeLion출력
+print("Hello, world!")
+print("LikeLion")
+
+#3 사칙연산(+,-,*,/) 해보기
+print(3+7)
+print(3-7)
+print(3*7)
+print(3/7)
+
+#4 그 외 연산 해보기
+#나눗셈 후 소수점 연산자 버리기 : //
+print(7//3)
+#나눗셈 후 나머지를 표시하기 : %
+print(7%3)
+#거듭제곱 : **
+print(2**3)
+#값을 정수로 만들기 : int()
+print(int(3.3))
+"""
+
+#9 몫과 나머지 함께 구하기 : divmod()
+print(divmod(5,2))
+
+#10     진법 표시하기
+# 2진법 : 숫자 앞에 0b붙임
+# 8진법 : 숫자 앞에 0o붙음
+# 16진법 : 숫자 앞에 0x또는 0X붙임
